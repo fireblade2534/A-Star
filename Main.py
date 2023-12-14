85c3b9a219878c626c23001b46a1fc5258493548
Fireblade2534 <77405729+fireblade2534@users.noreply.github.com>
2023-12-14 18:24:08 +0000

    djd

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import random
 import math
+import time
 class Grid:
     def __init__(self,Width:int,Height:int):
         self.Width=Width
@@ -23,7 +24,19 @@ class AStar:
     def GetLowestOpenCost(self):
         return list(filter(lambda X: not X[1]["Checked"],sorted(self.ExploredList.items(),key=lambda X: X[1]["CostFull"])))
 
-    def GeneratePath(self,StartID,TargetID,DTWeight:float=1.5,DBWeight:float=1):
+    def CompilePath(self):
+        CurrentSquare=self.TargetID
+        Path=[]
+        while True:
+            Path.append(CurrentSquare)
+            if CurrentSquare == self.StartID:
+                return Path[::-1]
+            CurrentSquare=self.ExploredList[CurrentSquare]["PreviousPlace"]
+            
+            
+
+
+    def GeneratePath(self,StartID,TargetID,DTWeight:float=1.5,DBWeight:float=1,AnimatePathing:bool=False):
         self.StartID=StartID
         self.TargetID=TargetID
         self.ExploredList={}
@@ -33,17 +46,34 @@ class AStar:
         Steps=0
         while Steps < 100:
             Lowest=self.GetLowestOpenCost()
+
             if len(Lowest) > 0:
                 Lowest=Lowest[0]
+                if AnimatePathing:
+                    time.sleep(0.1)
+                self.RefrenceFuntions["Render"](list(self.ExploredList.keys()),SearchLocation=Lowest[0])
+                #print(self.ExploredList)
                 CheckPositions=self.RefrenceFuntions["NeighbourSquares"](Lowest[0])
-                for ChekingPosition,CheckWeight in CheckPositions.items():
-                    DistanceToOld=self.RefrenceFuntions["Distance"](Lowest[0],CheckPositions)
-                    if ChekingPosition in self.ExploredList:
-                        
+                for CheckingPosition,CheckWeight in CheckPositions.items():
+                    DistanceToOld=self.RefrenceFuntions["Distance"](Lowest[0],CheckingPosition)
+                    NewDistanceToStart=self.ExploredList[Lowest[0]]["DistanceFromStart"] + DistanceToOld
+                    if CheckingPosition in self.ExploredList:
+                        if NewDistanceToStart < self.ExploredList[CheckingPosition]["DistanceFromStart"]:
+                            self.ExploredList[CheckingPosition]={"CostFull":(self.RefrenceFuntions["Distance"](CheckingPosition,TargetID) * DTWeight) + (NewDistanceToStart * DBWeight) + CheckWeight,"DistanceFromStart":NewDistanceToStart,"PreviousPlace":Lowest[0],"Checked":self.ExploredList[CheckingPosition]["Checked"]}
                     else:
-                        self.ExploredList[ChekingPosition]={"CostFull":(self.RefrenceFuntions["Distance"](ChekingPosition,TargetID) * DTWeight) + self,"DistanceFromStart":0,"PreviousPlace":StartID,"Checked":False}
-                print(CheckPositions)
-                print(Lowest)
+                        self.ExploredList[CheckingPosition]={"CostFull":(self.RefrenceFuntions["Distance"](CheckingPosition,TargetID) * DTWeight) + (NewDistanceToStart * DBWeight) + CheckWeight,"DistanceFromStart":NewDistanceToStart,"PreviousPlace":Lowest[0],"Checked":False}
+
+                    if CheckingPosition == self.TargetID:
+                        #print("DONE")
+                        FinalPath=self.CompilePath()
+                        self.RefrenceFuntions["Render"](list(self.ExploredList.keys()),Path=FinalPath,SearchLocation=Lowest[0])
+                        return
+
+                self.ExploredList[Lowest[0]]["Checked"]=True
+               
+
+                #print(CheckPositions)
+                #print(Lowest)
             Steps+=1
 
 
@@ -51,7 +81,7 @@ class GridAStar2D:
     def Distance(self,Position1:tuple[int],Position2:tuple[int]):
         if self.AllowDiagonals:
             return math.sqrt(((Position1[0] - Position2[0]) ** 2) + ((Position1[1] - Position2[1]) ** 2))
-        return (Position1[0] - Position2[0]) + (Position1[1] - Position2[1])
+        return abs(Position1[0] - Position2[0]) + abs(Position1[1] - Position2[1])
     
     def Weight(self,Position:tuple[int]):
         return self.Grid.Board[Position[1]][Position[0]]
@@ -62,17 +92,41 @@ class GridAStar2D:
         for Direction in Directions:
             NewPosition=(Position[0] + Direction[0],Position[1] + Direction[1])
             if NewPosition[0] < self.Grid.Width and NewPosition[0] >= 0 and NewPosition[1] < self.Grid.Height and NewPosition[1] >= 0:
-                OutputList[NewPosition]=self.Weight(NewPosition)
+                SquareWeight=self.Weight(NewPosition)
+                if SquareWeight != -1:
+                    OutputList[NewPosition]=SquareWeight
         return OutputList
     
+    def RenderGrid(self,ExploredSquares=[],Path=[],SearchLocation=(-1,-1)):
+        Out=""
+        for Y in range(0,self.Grid.Height):
+            for X in range(0,self.Grid.Width):
+                Final="⬜"
+                if self.Weight((X,Y)) == -1:
+                    Final="⬛"
+                if (X,Y) in ExploredSquares:
+                    Final="🟨"
+                if SearchLocation != (-1,-1) and SearchLocation == (X,Y):
+                    Final="🟦"
+                if (X,Y) in Path:
+                    Final="🟩"
+                Out+=Final
+
+            Out+="\n"
+
+        print(Out)
+
+
 
 
     def __init__(self,Grid:Grid,AllowDiagonals=False):
         self.Grid=Grid
         self.AllowDiagonals=AllowDiagonals
-        self.MainAStar=AStar({"Distance":self.Distance,"Weight":self.Weight,"NeighbourSquares":self.SquaresAround})
+        self.MainAStar=AStar({"Distance":self.Distance,"Weight":self.Weight,"NeighbourSquares":self.SquaresAround,"Render":self.RenderGrid})
     def GeneratePath(self,StartLocation:tuple[int]=(0,0),TargetLocation:tuple[int]=(0,0)):
-        self.MainAStar.GeneratePath(StartLocation,TargetLocation)
+        self.StartLocation=StartLocation
+        self.TargetLocation=TargetLocation
+        self.MainAStar.GeneratePath(StartLocation,TargetLocation,AnimatePathing=True)
 
 if __name__ == "__main__":
     A2D=GridAStar2D(Grid=Grid(10,10))
